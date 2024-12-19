749f8deec90809151609d97f947e036c9d90c69b
lunaeresta <123486151+lunaeresta@users.noreply.github.com>
2024-12-19 12:11:51 -0500

    added metaspades execution command

---
+++ scripts/metaspades_genome_assembly.sh
@@ -0,0 +1,29 @@
+# path to fastq data files and path to metaspades package in biohpc
+directory="/local1/workdir1/lej52/abx_recovery_metagenomics/processed_data"
+spades_package="/programs/spades/bin/spades.py"
+
+# for loop to execute metaspades for all sequence ID folders in the base directory folder
+for seqid in $(ls "$directory"); do
+    input_dir="${directory}/${seqid}"
+    output_dir="${input_dir}/spades_output"
+
+    # confirm whether the right input files for metaspades exist in the sequence ID folder
+    if [[ -f "${input_dir}/${seqid}.trim_1.fastq" && -f "${input_dir}/${seqid}.trim_2.fastq" && \
+          -f "${input_dir}/${seqid}.singleton_1.fastq" && -f "${input_dir}/${seqid}.singleton_2.fastq" ]]; then
+
+        # run metaspades
+        $spades_package --meta \
+            --pe-1 1 "${input_dir}/${seqid}.trim_1.fastq" \
+            --pe-2 1 "${input_dir}/${seqid}.trim_2.fastq" \
+            --pe-s 1 "${input_dir}/${seqid}.singleton_1.fastq" \
+            --pe-s 1 "${input_dir}/${seqid}.singleton_2.fastq" \
+            -k 21,33,55,77 \
+            -t 50 \
+            -m 125 \
+            -o "${output_dir}"
+
+        echo "successfully completed metaspades for ${seqid}"
+    else
+        echo "unable to locate fastq file(s) in ${seqid}"
+    fi
+done
