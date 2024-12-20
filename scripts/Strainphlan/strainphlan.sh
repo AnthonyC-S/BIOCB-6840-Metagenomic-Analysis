5740a311b9d42dd42f94b72b8bc5146976020f03
YangCheng7 <yc2785@cornell.edu>
2024-12-19 22:53:53 -0500

    Update strainphlan.sh

--- scripts/Strainphlan/strainphlan.sh
+++ scripts/Strainphlan/strainphlan.sh
@@ -1,11 +1,3 @@
-#!/bin/bash
-#SBATCH --job-name=bowtie2      	     # Job name
-#SBATCH --mail-type=FAIL            	 # Mail events (NONE, BEGIN, END, FAIL, ALL)
-#SBATCH --mail-user=yc2785@cornell.edu # Where to send mail
-#SBATCH --ntasks=1                   	 # Run a single task
-#SBATCH --mem=50gb                    # Job Memory
-#SBATCH --time=12:00:00             	 # Time limit hrs:min:sec
-#SBATCH --output=%j.log		    	       # Standard output and error log
 
 mkdir -p output
 strainphlan -s consensus_markers/*.json.bz2 -m db_markers/t__SGB7041.fna -r <reference genome> -o output -n 8 -c t__SGB7041 --mutation_rates 
